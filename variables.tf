e978a63447f3511523cee915ca9d0e6d547b4977
Bruno Matias <brunomatias3@gmail.com>
2024-09-25 20:32:12 -0300

    rds tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" { 
+    type = string 
+    default = ""
+}
+
+variable "vpc_id" { 
+    type = string
+    default = ""
+}
+
+variable "project" {
+    type = string
+    default = ""
+}
+
+variable "env" {
+    type = string
+    default = ""
+}
+
+variable "username" {
+  description = "Username for the master DB user"
+  type        = string
+}
