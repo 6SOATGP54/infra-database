bc7967020073747e51adfff4e9c6017c3129e941
Lucas <lucas.santiago@sgroup.com.br>
2024-09-26 20:32:06 -0300

    Adicionado instance

--- aurora-postgres.tf
+++ aurora-postgres.tf
@@ -1,20 +1,27 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"  # Substitua pela região que você deseja usar
+}
+provider "aws" {
+  region  = "us-east-1"
+}
 resource "aws_rds_cluster" "postgresql" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-postgresql"
-  availability_zones      = ["us-east-1a","us-east-1b","us-east-1c"]
+  availability_zones      = ["us-east-1a", "us-east-1b", "us-east-1c"]
   database_name           = "food"
   master_username         = "food"
   master_password         = "mV3&04I}Pt"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot = true
+  skip_final_snapshot     = true
 }
-
 
 resource "aws_rds_cluster_instance" "aurora_instance" {
   count              = 1
   identifier         = "aurora-instance-demo"
-  cluster_identifier = aws_rds_cluster.aurora_postgresql.id
+  cluster_identifier = aws_rds_cluster.postgresql.id
   instance_class     = "db.r5.large"
   engine             = "aurora-postgresql"
 }
@@ -38,7 +45,7 @@ resource "aws_security_group" "aurora_sg" {
 }
 
 resource "aws_subnet" "aurora_subnet_1" {
-  vpc_id            = "vpc-05ed0e14e65d479e1" # Substitua pelo seu VPC ID
+  vpc_id            = "vpc-05ed0e14e65d479e1"  # Substitua pelo seu VPC ID
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
 }
@@ -55,7 +62,7 @@ resource "aws_subnet" "aurora_subnet_3" {
   cidr_block        = "10.0.3.0/24"
 }
 
-resource "aws_rds_subnet_group" "aurora_subnet_group" {
+resource "aws_db_subnet_group" "aurora_subnet_group" {
   name       = "aurora-subnet-group"
   subnet_ids = [
     aws_subnet.aurora_subnet_1.id,
