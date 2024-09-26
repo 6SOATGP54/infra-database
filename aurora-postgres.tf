1f70ed556ac4a140bac17c2e17d2c5694d1b50d2
Lucas <lucas.santiago@sgroup.com.br>
2024-09-25 21:25:22 -0300

    teste

--- aurora-postgres.tf
+++ aurora-postgres.tf
@@ -4,7 +4,7 @@ resource "aws_rds_cluster" "postgresql" {
   availability_zones      = ["us-east-1a","us-east-1b","us-east-1c"]
   database_name           = "food"
   master_username         = "food"
-  master_password         = "mV+3&04I/}Pt"
+  master_password         = "mV3&04I}Pt"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }
