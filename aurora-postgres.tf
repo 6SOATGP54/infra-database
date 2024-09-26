aebb0ec7d97ea02d96e1cc202c18802140e4f187
Lucas <lucas.santiago@sgroup.com.br>
2024-09-25 21:23:21 -0300

    teste

--- aurora-postgres.tf
+++ aurora-postgres.tf
@@ -1,7 +1,7 @@
 resource "aws_rds_cluster" "postgresql" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-postgresql"
-  availability_zones      = ["us-east-1"]
+  availability_zones      = ["us-east-1a","us-east-1b","us-east-1c"]
   database_name           = "food"
   master_username         = "food"
   master_password         = "mV+3&04I/}Pt"
