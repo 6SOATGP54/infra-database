ac32e458162df7fa506aa44cc660a8261c8e2461
Lucas <lucas.santiago@sgroup.com.br>
2024-09-26 20:27:12 -0300

    Adicionado instance

--- aurora-postgres.tf
+++ aurora-postgres.tf
@@ -7,4 +7,59 @@ resource "aws_rds_cluster" "postgresql" {
   master_password         = "mV3&04I}Pt"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot = true
+}
+
+
+resource "aws_rds_cluster_instance" "aurora_instance" {
+  count              = 1
+  identifier         = "aurora-instance-demo"
+  cluster_identifier = aws_rds_cluster.aurora_postgresql.id
+  instance_class     = "db.r5.large"
+  engine             = "aurora-postgresql"
+}
+
+resource "aws_security_group" "aurora_sg" {
+  name = "aurora-sg"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_subnet" "aurora_subnet_1" {
+  vpc_id            = "vpc-05ed0e14e65d479e1" # Substitua pelo seu VPC ID
+  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "aurora_subnet_2" {
+  vpc_id            = "vpc-05ed0e14e65d479e1"
+  availability_zone = "us-east-1b"
+  cidr_block        = "10.0.2.0/24"
+}
+
+resource "aws_subnet" "aurora_subnet_3" {
+  vpc_id            = "vpc-05ed0e14e65d479e1"
+  availability_zone = "us-east-1c"
+  cidr_block        = "10.0.3.0/24"
+}
+
+resource "aws_rds_subnet_group" "aurora_subnet_group" {
+  name       = "aurora-subnet-group"
+  subnet_ids = [
+    aws_subnet.aurora_subnet_1.id,
+    aws_subnet.aurora_subnet_2.id,
+    aws_subnet.aurora_subnet_3.id
+  ]
 }
